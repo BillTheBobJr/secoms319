0965034dd368f8f2489d882bdd15a773701d13c7
anc1 <anc1@iastate.edu>
2023-03-07 14:55:48 -0600

    Added javascript and json files

---
+++ index.js
@@ -0,0 +1,19 @@
+var jsonData;
+
+
+fetch("./data.json")
+    .then(response => response.json())
+    .then(data => jsonData = data);
+
+
+
+
+function loadPage1(){
+
+}
+
+
+function loadPage2(){
+
+}
+
