1c801f17e715143590f7d1699c4bc25b3457f88b
sdunham01 <sdunhammc@gmail.com>
2023-03-09 21:58:59 -0600

    add basic modal when view button is clicked

--- index.js
+++ index.js
@@ -1,35 +1,90 @@
 var jsonData;
 
-async function startPage1(){
+async function startPage1() {
     await fetch("./data.json")
-    .then(response => response.json())
-    .then(data => jsonData = data);
+        .then(response => response.json())
+        .then(data => jsonData = data);
     loadPage1();
 }
 
-function loadPage1(){
+function loadPage1() {
 
 }
 
 
-async function startPage2(){
+
+function prepRent() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    var closeBtn = document.getElementById("closeBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    closeBtn.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function openModal(carname){
+    prepRent();
+    var modal = document.getElementById("myModal");
+    var modalTitle = document.getElementById("modalTitle");
+    var modalDescription = document.getElementById("modalDescription");
+
+    let rentData = jsonData.page2.find((car) => car.carName == carname);
+
+    modalTitle.innerHTML = carname;
+    
+    
+    modalDescription.innerHTML = rentData.description;
+
+
+
+    modal.style.display = "block";
+
+
+}
+
+function loadRent(carName) {
+
+}
+
+
+async function startPage2() {
     await fetch("./data.json")
-    .then(response => response.json())
-    .then(data => jsonData = data);
+        .then(response => response.json())
+        .then(data => jsonData = data);
     loadPage2();
 }
 
-function loadPage2(){
-
+function loadPage2() {
+    
     let page2 = document.getElementById("cards");
     page2.innerHTML = "";
-    for(let card of jsonData["page2"]){
+    for (let card of jsonData["page2"]) {
         let name = card["carName"];
         let description = card["description"];
         let image = card["image"];
         let price = card["price"];
-        page2.innerHTML += 
-        `<div class = "col">
+        page2.innerHTML +=
+            `<div class = "col">
             <div class="card shadow-sm">
                 <img src="${image}" alt="${name}">
                 <div class="card-header">
@@ -39,9 +94,7 @@ function loadPage2(){
                     <p class="card-text">${description}</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <a href="${image}">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            </a>
+                                <button onclick="javascript:openModal('${name}')" type="button" class="btn btn-sm btn-outline-secondary">View</button>
                         </div>
                         <small class="text-muted">\$${price}/day</small>
                     </div>
@@ -53,16 +106,16 @@ function loadPage2(){
 
 }
 
-function page2SortBy(field, order = 1){
+function page2SortBy(field, order = 1) {
     let pos;
     let temp;
     let arr = jsonData["page2"];
-    for(let i = 0; i < arr.length - 1; i++){
+    for (let i = 0; i < arr.length - 1; i++) {
         pos = i;
-        for(let p = i; p < arr.length; p++){
-            let tester = (Number.isInteger(arr[p][field])) ? order * arr[p][field]: arr[p][field];
-            let testy = (Number.isInteger(arr[pos][field])) ? order * arr[pos][field]: arr[pos][field];
-            if(tester < testy){
+        for (let p = i; p < arr.length; p++) {
+            let tester = (Number.isInteger(arr[p][field])) ? order * arr[p][field] : arr[p][field];
+            let testy = (Number.isInteger(arr[pos][field])) ? order * arr[pos][field] : arr[pos][field];
+            if (tester < testy) {
                 pos = p;
             }
         }
