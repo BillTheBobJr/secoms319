f89b434e92ad464c0026a0de59428ff2ded4e180
anc1 <anc1@iastate.edu>
2023-04-11 03:19:39 -0500

    Finished styling and functionality, just need to update json and make sure pictures are read in

--- index.js
+++ index.js
@@ -23,8 +23,23 @@ function Main() {
     email: "",
     creditcard: "",
     address1: "",
-    address2: ""
+    address2: "",
+    city: "",
+    state: "",
+    zip: ""
   });
+
+
+  let searchButton = document.getElementById("searchQuery");
+
+  if (searchButton) {
+    searchButton.addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("searchBtn").click();
+      }
+    });
+  }
 
 
   useEffect(() => {
@@ -96,7 +111,11 @@ function Main() {
       email: event.target.email.value,
       creditcard: event.target.creditCard.value,
       address1: event.target.address1.value,
-      address2: event.target.address2.value
+      address2: event.target.address2.value,
+      city: event.target.city.value,
+      state: event.target.state.value,
+      zip: event.target.zip.value
+
     });
 
     changeView("confirmation");
@@ -108,24 +127,28 @@ function Main() {
     changeView("browse");
   };
 
-
+  const leaveBrowse = () => {
+    document.getElementById("searchQuery").value = "";
+    searchProducts();
+    changeView("cart");
+  }
 
   const changeView = (newView) => {
     setView(newView);
   };
 
   return (
-    <div className="Main">
+    <div className="Main" style={{ marginLeft: '1%', marginRight: '1%' }}>
       {view === "browse" && (
         <div>
           <h1>Browse Products</h1>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <form class="form-inline my-2 my-lg-0 mr-auto">
+            <form className="form-inline my-2 my-lg-0 mr-auto">
               <input className="form-control mr-sm-2" placeholder="Search" aria-label="Search" id="searchQuery"></input>
-              <button className="btn btn-outline-success my-2 my-sm-0" type="button" onClick={() => searchProducts()}>Search</button>
+              <button className="btn btn-outline-success my-2 my-sm-0" type="button" onClick={() => searchProducts()} id="searchBtn">Search</button>
             </form>
             <form className="form-inline my-2 my-lg-0">
-              <button onClick={() => changeView("cart")} className="btn btn-outline-primary">Checkout</button>
+              <button onClick={() => leaveBrowse()} className="btn btn-outline-primary">Checkout</button>
             </form>
           </nav>
           <div className="products card-columns">
@@ -147,46 +170,66 @@ function Main() {
       {view === "cart" && (
         <div>
           <h1>Cart</h1>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button onClick={() => changeView("browse")} className="btn btn-outline-success">Return</button>
+          </nav>
           <div className="cart-items">
-            {cart.map((product) => (
-              <div key={product.id} className="cart-item">
-                <h2>{product.name}</h2>
-                <p>Quantity: {product.quantity}</p>
-                <p>Total: ${product.price * product.quantity}</p>
-                <button onClick={() => removeFromCart(product)}>Remove</button>
-              </div>
-            ))}
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Price</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((product) => (
+                  <tr key={product.id} className="cart-item">
+                    <td>{product.name}</td>
+                    <td>Quantity: {product.quantity}</td>
+                    <td>Total: ${product.price * product.quantity}</td>
+                    <td><button onClick={() => removeFromCart(product)} className="btn btn-light">Remove</button></td>
+                  </tr>
+                ))}
+                <tr>
+                  <td></td>
+                  <td>Total</td>
+                  <td>${getCartTotal().toFixed(2)}</td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <p>Total: ${getCartTotal().toFixed(2)}</p>
-
-          <button onClick={() => changeView("browse")}>Back to browse</button>
           <form onSubmit={confirmationPageGo}>
-            <label>
-              Name:
-              <input type="text" name="name" pattern = "[A-Za-z ]+" required/>
-            </label>
+            <div className="row">
+              <div className="col">
+                <input placeholder="Name" type="text" name="name" pattern="[A-Za-z ]+" className="form-control" required />
+              </div>
+              <div className="col">
+                <input placeholder="Email" type="email" name="email" className="form-control" required />
+              </div>
+            </div>
             <br />
-            <label>
-              Email:
-              <input type="email" name="email" required/>
-            </label>
+            <input type="password" name="creditCard" pattern="\d{16}" className="form-control" placeholder="Credit Card" required />
             <br />
-            <label>
-              Credit Card:
-              <input type="text" name="creditCard" pattern = "\d{16}" required/>
-            </label>
+            <input type="text" name="address1" placeholder="Address Line 1" className="form-control" required />
             <br />
-            <label>
-              Address:
-              <input type="text" name="address1" required/>
-            </label>
+            <input type="text" name="address2" placeholder="Address Line 2" className="form-control" />
             <br />
-            <label>
-              Address Line 2:
-              <input type="text" name="address2"/>
-            </label>
+            <div className="row">
+              <div className="col">
+                <input placeholder="City" type="text" name="city" pattern="[A-Za-z ]+" className="form-control" required />
+              </div>
+              <div className="col">
+                <input placeholder="State" type="text" name="state" pattern="[A-Za-z ]+" className="form-control" required />
+              </div>
+              <div className="col">
+                <input placeholder="Zip" type="text" name="zip" className="form-control" pattern="\d{5}" required />
+              </div>
+            </div>
             <br />
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" className="btn btn-outline-primary" />
           </form>
 
         </div>
@@ -194,28 +237,54 @@ function Main() {
       {view === "confirmation" && (
         <div>
           <h2>Confirmation</h2>
-          <ul>
-            {cart.map((item) => (
-              <li key={item.id}>
-                {item.name} - Quantity: {item.quantity} - Price: $
-                {(item.price * item.quantity).toFixed(2)}
-              </li>
-            ))}
-          </ul>
-          <p>Total: ${getCartTotal().toFixed(2)}</p>
-          <h3>User Information</h3>
-          <p>Name: {userDetails.name}</p>
-          <p>Email: {userDetails.email}</p>
-          <p>Card: {getCardNumber()}</p>
-          <p>Address{userDetails.address2 && (" Line 1")}: {userDetails.address1}</p>
-          {userDetails.address2 && (<p>Address Line 2: {userDetails.address2}</p>)}
-          <button
-            onClick={() => {
-              leaveConfirmation()
-            }}
-          >
-            Back to Browse
-          </button>
+
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button onClick={() => { leaveConfirmation() }} className="btn btn-outline-success">Back to Browse</button>
+          </nav>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Breakdown</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.quantity} * ${item.price.toFixed(2)} = ${(item.price * item.quantity).toFixed(2)}</td>
+                </tr>
+              ))}
+              <tr>
+                <td>Total</td>
+                <td>${getCartTotal().toFixed(2)}</td></tr>
+            </tbody>
+          </table>
+          <table className="table">
+            <tbody>
+              <tr>
+                <td>Name: {userDetails.name}</td>
+                <td>Email: {userDetails.email}</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Card: {getCardNumber()}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Address{userDetails.address2 && (" Line 1")}: {userDetails.address1}</td>
+                <td></td>
+                <td></td>
+              </tr>
+              {userDetails.address2 && (<tr><td>Address Line 2: {userDetails.address2}</td><td></td><td></td></tr>)}
+              <tr>
+                <td>City: {userDetails.city}</td>
+                <td>State: {userDetails.state}</td>
+                <td>Zip: {userDetails.zip}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       )}
 
